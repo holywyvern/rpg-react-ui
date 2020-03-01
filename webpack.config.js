ee428b6711351911d55fddd5fa8ddbf32da89ebd
holywyvern <ramiro.rojo.cretta@gmail.com>
2020-03-01 16:45:58 -0300

    Build target using externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require("path");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   mode: "production",
   entry: ["./src/index.js"],
+  target: "node",
+  externals: [nodeExternals()],
   output: {
-    path: path.join(__dirname, "lib"),
-    publicPath: "/",
-    filename: "index.js"
+    path: path.join(__dirname, "dist"),
+    library: "rpg-react-ui",
+    libraryTarget: "commonjs"
   },
   module: {
     rules: [
